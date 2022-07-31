27593b84a9d5390aaac47bb5fa69cf0f18f1e4b6
greene5952 <96886480+greene5952@users.noreply.github.com>
2022-07-31 10:01:27 -0500

    color

--- pages/claims/index.js
+++ pages/claims/index.js
@@ -105,10 +105,10 @@ export default function Claim() {
               {(!currentUseState.isConnected) ?
                 <Col lg="6" md="6" className="align-self-center">
                   {(currentUseState.network == "rinkeby") ? <h3 style={{ color: "#fff" }}>DEMO ONLY RINKEBY</h3> : ""}
-                  <h3 className="title" style={{ color: "#e9e2d5"}}>
+                  <h3 className="title" style={{ color: "#ffffff"}}>
                     { process.env.pageHeader }
                   </h3>
-                  <h4 className="subtitle font-light" style={{ color: "#e9e2d5"}}>
+                  <h4 className="subtitle font-light" style={{ color: "#ffffff"}}>
                     { process.env.pageText }
                     <br />
                   </h4>
@@ -164,10 +164,10 @@ export default function Claim() {
                             </> :
                             <>
                               <hr />
-                              <h1 className="subtitle font-light" style={{ color: "#e9e2d5"}}>Communicating with the blockchain!</h1>
+                              <h1 className="subtitle font-light" style={{ color: "#ffffff"}}>Communicating with the blockchain!</h1>
                             </>}
                         </>
-                        : <h1 className="subtitle font-light" style={{ color: "#e9e2d5"}}>You are not on the whitelist</h1>
+                        : <h1 className="subtitle font-light" style={{ color: "#ffffff"}}>You are not on the whitelist</h1>
                       }
                       <a
                         onClick={() => walletBridge1.disconnect()}
@@ -185,7 +185,7 @@ export default function Claim() {
                       {currentUseState.hashHtml}
                     </>
                     :
-                    <h1 className="subtitle font-light" style={{ color: "#e9e2d5"}}>Claim mint is currently closed!</h1>
+                    <h1 className="subtitle font-light" style={{ color: "#ffffff"}}>Claim mint is currently closed!</h1>
                   }
                 </Col>
               }
